4885fa45ba6f2d7f8a51bf35d2470a5fdb7e718e
QingZe Luo <98981724+QZKasoma-Luo@users.noreply.github.com>
2024-08-12 03:11:42 -0700

    update

--- Assignment_5/src/attributes.h
+++ Assignment_5/src/attributes.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <Eigen/Core>
-using namespace Eigen;
+
 class VertexAttributes
 {
 public:
@@ -41,7 +41,7 @@ public:
 class FrameBufferAttributes
 {
 public:
-    FrameBufferAttributes(double r = 0, double g = 0, double b = 0, double a = 1)
+    FrameBufferAttributes(double r = 0, double g = 0, double b = 0, double a = 255)
     {
         color << r, g, b, a;
     }
@@ -52,21 +52,7 @@ public:
 class UniformAttributes
 {
 public:
-    // Constructor
-    UniformAttributes() : mvpMatrix(Matrix4d::Identity()) {} // Initialize with the identity matrix
-
-    // Getter for the transformation matrix
-    const Matrix4d &getMvpMatrix() const
-    {
-        return mvpMatrix;
-    }
-
-    // Setter for the transformation matrix
-    void setMvpMatrix(const Matrix4d &matrix)
-    {
-        mvpMatrix = matrix;
-    }
-
-private:
-    Matrix4d mvpMatrix; // Stores the Model-View-Projection matrix
+    Eigen::Matrix4d camera_view_transformed; // store the camera view matrix
+    Eigen::Matrix4d mvp_matrix;              // store the model view projection matrix
+    Eigen::Matrix4d projection_matrix;       // store the projection matrix
 };
--- Assignment_5/src/main.cpp
+++ Assignment_5/src/main.cpp
@@ -26,7 +26,7 @@ const double near_plane = 1.5; // AKA focal length
 const double far_plane = near_plane * 100;
 const double field_of_view = 0.7854; // 45 degrees
 const double aspect_ratio = 1.5;
-const bool is_perspective = true;
+const bool is_perspective = false;
 const Vector3d camera_position(0, 0, 3);
 const Vector3d camera_gaze(0, 0, -1);
 const Vector3d camera_top(0, 1, 0);
@@ -101,45 +101,47 @@ void setup_scene()
 void build_uniform(UniformAttributes &uniform)
 {
     // TODO: setup uniform
-
-    // TODO: setup camera, compute w, u, v
-    Vector3d w = -(camera_gaze.normalized());
+    Vector3d w = -camera_gaze.normalized();
     Vector3d u = camera_top.cross(w).normalized();
     Vector3d v = w.cross(u);
-
+    // TODO: setup camera, compute w, u, v
     // TODO: compute the camera transformation
-    Matrix4d camera_transformation = Matrix4d::Zero();
-    camera_transformation.block<3, 1>(0, 0) = u;
-    camera_transformation.block<3, 1>(0, 1) = v;
-    camera_transformation.block<3, 1>(0, 2) = w;
-    camera_transformation.block<3, 1>(0, 3) = camera_position;
-    camera_transformation(3, 3) = 1.0;
+    Matrix4d camera_view_transformed;
+    camera_view_transformed.block<3, 1>(0, 0) = u;                // u vector
+    camera_view_transformed.block<3, 1>(0, 1) = v;                // v vector
+    camera_view_transformed.block<3, 1>(0, 2) = w;                // w vector
+    camera_view_transformed.block<3, 1>(0, 3) = -camera_position; // camera position
+    camera_view_transformed(3, 3) = 1.0;
+    uniform.camera_view_transformed = camera_view_transformed;
 
     // TODO: setup projection matrix
-    double t = near_plane * tan(field_of_view / 2);
-    double f = -far_plane;
-    double n = -near_plane;
-    double r = t * aspect_ratio;
-    double l = -r;
-    double b = -t;
+
     Matrix4d P;
     if (is_perspective)
     {
-        // TODO setup prespective camera
-        P << 2 * near_plane / (r - l), 0, (r + l) / (r - l), 0,
-            0, 2 * near_plane / (t - b), (t + b) / (t - b), 0,
-            0, 0, (f + n) / (n - f), 2 * f * n / (n - f),
+        double scale = 1.0 / tan(field_of_view / 2.0);
+        P << scale / aspect_ratio, 0, 0, 0,
+            0, scale, 0, 0,
+            0, 0, -(far_plane + near_plane) / (far_plane - near_plane), -2 * far_plane * near_plane / (far_plane - near_plane),
             0, 0, -1, 0;
+        uniform.projection_matrix = P;
     }
     else
     {
-        P << 2 / (r - l), 0, 0, -(r + l) / (r - l),
-            0, 2 / (t - b), 0, -(t + b) / (t - b),
-            0, 0, -2 / (f - n), -(f + n) / (f - n),
+        double left = -1.0 * aspect_ratio;
+        double right = 1.0 * aspect_ratio;
+        double bottom = -1.0;
+        double top = 1.0;
+
+        P << 2.0 / (right - left), 0, 0, -(right + left) / (right - left),
+            0, 2.0 / (top - bottom), 0, -(top + bottom) / (top - bottom),
+            0, 0, -2.0 / (far_plane - near_plane), -(far_plane + near_plane) / (far_plane - near_plane),
             0, 0, 0, 1;
+        uniform.projection_matrix = P;
     }
 
-    uniform.setMvpMatrix(P * camera_transformation.inverse());
+    Matrix4d model_matrix = Matrix4d::Identity(); // 假设模型矩阵为单位矩阵
+    uniform.mvp_matrix = uniform.projection_matrix * uniform.camera_view_transformed * model_matrix;
 }
 
 void simple_render(Eigen::Matrix<FrameBufferAttributes, Eigen::Dynamic, Eigen::Dynamic> &frameBuffer)
@@ -151,10 +153,9 @@ void simple_render(Eigen::Matrix<FrameBufferAttributes, Eigen::Dynamic, Eigen::D
     program.VertexShader = [](const VertexAttributes &va, const UniformAttributes &uniform)
     {
         // TODO: fill the shader
-        VertexAttributes out_va;
-        // Divide all components by the w component for perspective division
-        out_va.position = uniform.getMvpMatrix() * va.position;
-        return out_va;
+        VertexAttributes shader_out;
+        shader_out.position = uniform.projection_matrix * uniform.camera_view_transformed * va.position;
+        return shader_out;
     };
 
     program.FragmentShader = [](const VertexAttributes &va, const UniformAttributes &uniform)
@@ -166,58 +167,23 @@ void simple_render(Eigen::Matrix<FrameBufferAttributes, Eigen::Dynamic, Eigen::D
     program.BlendingShader = [](const FragmentAttributes &fa, const FrameBufferAttributes &previous)
     {
         // TODO: fill the shader
-        int r = static_cast<int>(fa.color[0] * 255);
-        int g = static_cast<int>(fa.color[1] * 255);
-        int b = static_cast<int>(fa.color[2] * 255);
-        int a = static_cast<int>(fa.color[3] * 255);
-        return FrameBufferAttributes(r, g, b, a);
+        return FrameBufferAttributes(fa.color[0] * 255, fa.color[1] * 255, fa.color[2] * 255, fa.color[3] * 255);
     };
 
-    // std::vector<VertexAttributes> vertex_attributes;
-    // TODO: build the vertex attributes from vertices and facets
     std::vector<VertexAttributes> vertex_attributes;
-    vertex_attributes.reserve(3 * facets.rows()); // 根据需要调整大小，避免在循环中分配内存
-
-    for (int i = 0; i < facets.rows(); ++i)
+    // TODO: build the vertex attributes from vertices and facets
+    for (int i = 0; i < vertices.rows(); ++i)
     {
-        vertex_attributes.clear();
-        vertex_attributes.reserve(3);
-
-        for (int j = 0; j < 3; ++j)
-        {
-            int idx = facets(i, j);
-            Eigen::Vector3d v = vertices.row(idx);
-            vertex_attributes.emplace_back(v[0], v[1], v[2]);
-        }
-
-        rasterize_triangles(program, uniform, vertex_attributes, frameBuffer);
+        vertex_attributes.emplace_back(vertices(i, 0), vertices(i, 1), vertices(i, 2));
     }
+    rasterize_triangles(program, uniform, vertex_attributes, frameBuffer);
 }
 
-Eigen::Matrix4d compute_rotation(const double alpha)
+Matrix4d compute_rotation(const double alpha)
 {
     // TODO: Compute the rotation matrix of angle alpha on the y axis around the object barycenter
-    Eigen::Matrix4d res;
-    double barycenter_x = 1.0, barycenter_y = 2.0, barycenter_z = 3.0;
+    Matrix4d res;
 
-    Eigen::Matrix4d translate_to_origin = Eigen::Matrix4d::Identity();
-    translate_to_origin(0, 3) = -barycenter_x;
-    translate_to_origin(1, 3) = -barycenter_y;
-    translate_to_origin(2, 3) = -barycenter_z;
-
-    Eigen::Matrix4d translate_back = Eigen::Matrix4d::Identity();
-    translate_back(0, 3) = barycenter_x;
-    translate_back(1, 3) = barycenter_y;
-    translate_back(2, 3) = barycenter_z;
-
-    Eigen::Matrix4d rotation = Eigen::Matrix4d::Identity();
-    rotation(0, 0) = cos(alpha);
-    rotation(0, 2) = sin(alpha);
-    rotation(2, 0) = -sin(alpha);
-    rotation(2, 2) = cos(alpha);
-
-    // Combine transformations
-    res = translate_back * rotation * translate_to_origin;
     return res;
 }
 
@@ -228,14 +194,13 @@ void wireframe_render(const double alpha, Eigen::Matrix<FrameBufferAttributes, E
     Program program;
 
     Matrix4d trafo = compute_rotation(alpha);
-    uniform.setMvpMatrix(uniform.getMvpMatrix() * trafo);
 
     program.VertexShader = [](const VertexAttributes &va, const UniformAttributes &uniform)
     {
         // TODO: fill the shader
-        VertexAttributes va_out;
-        va_out.position = uniform.getMvpMatrix() * va.position;
-        return va_out;
+        VertexAttributes shader_out;
+        shader_out.position = uniform.projection_matrix * uniform.camera_view_transformed * va.position;
+        return shader_out;
     };
 
     program.FragmentShader = [](const VertexAttributes &va, const UniformAttributes &uniform)
@@ -254,37 +219,6 @@ void wireframe_render(const double alpha, Eigen::Matrix<FrameBufferAttributes, E
 
     // TODO: generate the vertex attributes for the edges and rasterize the lines
     // TODO: use the transformation matrix
-
-    // Estimate number of lines: 3 lines per triangle
-    vertex_attributes.reserve(facets.rows() * 3 * 2); // Each triangle has 3 edges, 2 vertices per edge
-
-    // Apply the transformation matrix directly in the loop
-    Matrix4d transform = uniform.getMvpMatrix(); // Transformation matrix from uniform
-
-    for (int i = 0; i < facets.rows(); i++)
-    {
-        // get triangle
-        Vector3i triangle = facets.row(i);
-
-        // get vertices of triangle
-        Vector3d a = vertices.row(triangle[0]);
-        Vector3d b = vertices.row(triangle[1]);
-        Vector3d c = vertices.row(triangle[2]);
-
-        // add lines of triangle to vertex attributes
-
-        // ab line
-        vertex_attributes.push_back(VertexAttributes(a[0], a[1], a[2]));
-        vertex_attributes.push_back(VertexAttributes(b[0], b[1], b[2]));
-
-        // ac line
-        vertex_attributes.push_back(VertexAttributes(a[0], a[1], a[2]));
-        vertex_attributes.push_back(VertexAttributes(c[0], c[1], c[2]));
-
-        // bc line
-        vertex_attributes.push_back(VertexAttributes(b[0], b[1], b[2]));
-        vertex_attributes.push_back(VertexAttributes(c[0], c[1], c[2]));
-    }
 
     rasterize_lines(program, uniform, vertex_attributes, 0.5, frameBuffer);
 }
@@ -322,40 +256,6 @@ void flat_shading(const double alpha, Eigen::Matrix<FrameBufferAttributes, Eigen
     std::vector<VertexAttributes> vertex_attributes;
     // TODO: compute the normals
     // TODO: set material colors
-
-    for (int i = 0; i < facets.rows(); i++)
-    {
-        // get triangle
-        Vector3i triangle = facets.row(i);
-
-        // get vertices of triangle
-        Vector3d a = vertices.row(triangle[0]);
-        Vector3d b = vertices.row(triangle[1]);
-        Vector3d c = vertices.row(triangle[2]);
-
-        // compute normal of triangle
-        Vector3d N = ((b - a).cross(c - a)).normalized();
-
-        // create vertices
-        VertexAttributes va(a[0], a[1], a[2]);
-        VertexAttributes vb(b[0], b[1], b[2]);
-        VertexAttributes vc(c[0], c[1], c[2]);
-
-        // add normals to vertices
-        va.normal = N;
-        vb.normal = N;
-        vc.normal = N;
-
-        // set material colors
-        va.color << 1, 1, 1;
-        vb.color << 1, 1, 1;
-        vc.color << 1, 1, 1;
-
-        // add to vertex attributes
-        vertex_attributes.push_back(va);
-        vertex_attributes.push_back(vb);
-        vertex_attributes.push_back(vc);
-    }
 
     rasterize_triangles(program, uniform, vertex_attributes, frameBuffer);
 }
